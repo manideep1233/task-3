be98c9ec6550dad6536794a5f15819543ffbdaea
20BQ1A0526 <20BQ1A0526@vvit.net>
2024-03-01 19:33:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CurrentWeather from './components/current-weather/current-weather';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './Api';
 import { useState } from 'react';
 import Forecast from './components/forecast/forecast';
+import Location from './components/location/location';
 
 function App() {
   const [currentweather, setcurrentweather] = useState(null);
@@ -27,15 +28,24 @@ function App() {
       .catch((err) => console.log(err));
 
   }
-  console.log(forecast);
-  
-  return (
-    <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-      {currentweather && <CurrentWeather data={currentweather} />}
-      {forecast &&<Forecast data={forecast}/>}
-    </div>
-  );
+  const handleLocation = () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position)=> {
+        handleOnSearchChange({ value:`${position.coords.latitude} ${position.coords.longitude}`});
+    })
+  }
+    else {
+    alert('Geolocation is not supported by your browser');
+  }
+}
+return (
+  <div className="container">
+    <Search onSearchChange={handleOnSearchChange} />
+    <div className='location'><button onClick={handleLocation}><Location /></button></div>
+    {currentweather && <CurrentWeather data={currentweather} />}
+    {forecast && <Forecast data={forecast} />}
+  </div>
+);
 }
 
 export default App;
---
+++ src/components/location/location.js
@@ -0,0 +1,11 @@
+import PlaceIcon from '@mui/icons-material/Place';
+
+const Location =() =>{
+    return(
+        <>
+            <PlaceIcon/>
+        </>
+    )
+}
+
+export default Location
